c43e19106b884aa9f018095a337605fbbcc9249e
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-06 12:35:42 +0000

    Refactor tests

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -8,14 +8,8 @@ describe Bank do
     end
 
   describe '#deposit' do
-    it { is_expected.to respond_to(:deposit).with(1).argument }
 
-    it 'can deposit a specific amount' do
-      bank.deposit 100
-      expect(bank.balance).to eq 100
-    end
-
-    it 'updates balance' do
+    it 'updates balance when depositing a specific amount' do
       expect { bank.deposit 10 }.to change { bank.balance }.by 10
     end
 
