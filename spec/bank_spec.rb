6e9c539ab87be8eb52a4503a95d5c826bc68ae04
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-06 12:21:48 +0000

    Add deposit specific amount test

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -10,9 +10,15 @@ describe Bank do
   describe '#deposit' do
     it { is_expected.to respond_to(:deposit).with(1).argument }
 
+    it 'can deposit a specific amount' do
+      bank.deposit 100
+      expect(bank.balance).to eq 100
+    end
+
     it 'updates balance' do
       expect {bank.deposit 10 }.to change {bank.balance}.by 10
     end
+
 
   end
 
